ccb9a1c28d1c951f65454063949963af6cfa1b67
unknown <ale.disanso@gmail.com>
2022-09-22 19:59:28 -0300

    exclamacion

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -199,6 +199,8 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  var resultado = str + "!";
+  return resultado;
 }
 
 function combinarNombres(nombre, apellido) {
