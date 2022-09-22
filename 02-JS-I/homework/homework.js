1ca1af634507b9454348e47da6907d3c8dfa3b37
unknown <ale.disanso@gmail.com>
2022-09-22 19:32:04 -0300

    ej return 1 y 2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,16 +28,16 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  return "str"
+  return "str";
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  var resultado
-  x + y = resultado
-  return resultado
+  var resultado;
+  x + y = resultado;
+  return resultado;
 }
 
 function resta(x, y) {
