d357f3521b8ffbb4bb525f5a401292165c0e90f8
unknown <ale.disanso@gmail.com>
2022-09-22 20:52:05 -0300

    idioma

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -13,12 +13,21 @@ function obtenerMayor(x, y) {
   {
     return y;
   }
+  if (x === y)
+  {
+    return y;
+  }
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad > 18)
+  {
+    return "Allowed";
+  }
+  return "Not Allowed";
 }
   
 function conection(status) {
@@ -27,6 +36,15 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if (status === 1)
+  {
+    return "Online";
+  }
+  if (status === 2)
+  {
+    return "Away";
+  }
+  return "Offline";
 }
 
 function saludo(idioma) {
@@ -36,6 +54,23 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma === "aleman")
+  {
+    return "Guten Tag!";
+  }
+  if (idioma === "mandarin")
+  {
+    return "Ni Hao!";
+  }
+  if (idioma === "ingles")
+  {
+    return "Hello!";
+  }
+  if (idioma === undefined)
+  {
+    return "Hola!";
+  }
+  return "Hola!";
 }
 
 function colors(color) {
