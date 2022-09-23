ca252dd2032504fa76736408b328b344c033cf35
unknown <ale.disanso@gmail.com>
2022-09-22 21:03:43 -0300

    false/true

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -103,12 +103,27 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  switch (numero)
+  {
+    case 5:
+      "true";
+    case 10:
+      "true";
+    default:
+      "false";
+  }
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero < 50 && numero >20)
+  {
+    return "true";
+  }
+  else
+  return "false";
 }
 
 function esEntero(numero) {
